a4f9251339049033ecf8df43d2210988e2b32ece
Anwesh43 <anweshthecool0@gmail.com>
2021-07-13 00:38:14 +0530

    adding SBTHNode class

--- index.ts
+++ index.ts
@@ -171,3 +171,45 @@ class Animator {
         }
     }
 }
+
+class STBHNode {
+
+    prev : STBHNode 
+    next : STBHNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new STBHNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSTBHNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : STBHNode {
+        var curr : STBHNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
