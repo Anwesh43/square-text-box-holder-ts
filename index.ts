c1c39311121b194b38a9d740eb89d53c898d7ebc
Anwesh43 <anweshthecool0@gmail.com>
2021-07-13 00:40:19 +0530

    adding SquareTextBoxHolder class

--- index.ts
+++ index.ts
@@ -213,3 +213,26 @@ class STBHNode {
         return this 
     }
 }
+
+class SquareTextBoxHolder {
+
+    curr : STBHNode = new STBHNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+    
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
